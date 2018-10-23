94a84a571682b1cc6cbf662c5c180449740510aa
Bassam Mednini <Bassam.Mednini.1997@gmail.com>
2018-10-23 15:14:56 +0200

    Entfernt

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import App from './app';
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-ReactDOM.render(<App />, document.getElementById('root'));
